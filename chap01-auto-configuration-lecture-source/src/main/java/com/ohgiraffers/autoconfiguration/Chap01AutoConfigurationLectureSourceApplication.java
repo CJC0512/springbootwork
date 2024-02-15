3a67bcc0dc615a52d58760ef34f8ca261f94e3e8
CJC0512 <ehdchs2006@gmail.com>
2024-02-15 11:19:36 +0900

    springbootwork 시작 및 chap01 auto-configuration Proj. 생성

---
+++ chap01-auto-configuration-lecture-source/src/main/java/com/ohgiraffers/autoconfiguration/Chap01AutoConfigurationLectureSourceApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.autoconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap01AutoConfigurationLectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap01AutoConfigurationLectureSourceApplication.class, args);
+    }
+
+}
---
+++ chap01-auto-configuration-lecture-source/src/test/java/com/ohgiraffers/autoconfiguration/Chap01AutoConfigurationLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.autoconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap01AutoConfigurationLectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
