029bb05f40215ed9a57145928321101505119127
CJC0512 <ehdchs2006@gmail.com>
2024-02-16 11:36:54 +0900

    chap04 생성

---
+++ chap04-view-resolver-lecture-source/src/main/java/com/ohgiraffers/viewresolver/Chap04ViewResolverLectureSourceApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.viewresolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap04ViewResolverLectureSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap04ViewResolverLectureSourceApplication.class, args);
+	}
+
+}
