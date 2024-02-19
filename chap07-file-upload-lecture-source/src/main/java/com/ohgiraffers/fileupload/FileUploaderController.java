53de33e0654711eb51d94e250557b1103de355a4
CJC0512 <ehdchs2006@gmail.com>
2024-02-19 12:48:57 +0900

    chap07 fileupload single&multi 작성

--- chap07-file-upload-lecture-source/src/main/java/com/ohgiraffers/fileupload/FileUploaderController.java
+++ chap07-file-upload-lecture-source/src/main/java/com/ohgiraffers/fileupload/FileUploaderController.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.UUID;
+import java.util.*;
 
 @Controller
 public class FileUploaderController {
@@ -20,7 +20,7 @@ public class FileUploaderController {
     /* 설명. build된 파일 업로드 할 경로를 가져오기 위해 ResourceLoader 의존성 주입 받기 */
     @Autowired
     private ResourceLoader resourceLoader;
-    
+
     @PostMapping("single-file")
     public String singleFileUpload(@RequestParam MultipartFile singleFile,
                                    @RequestParam String singleFileDescription,
@@ -49,15 +49,15 @@ public class FileUploaderController {
         try {
             singleFile.transferTo(new File(filePath + "/" + savedName));
 
-            /* 설명. DB를 다녀오는 BL 구문 작성 (DB에 저장) */
+            /* 설명. DB를 다녀오는 BL(Business Logic) 구문 작성 (DB에 저장) */
             /* 설명. BL이 성공하면 화면의 재료를 RedirectAttributes로 담는다.(flashAttribute)*/
             rttr.addFlashAttribute("message", "파일 업로드 성공");
             rttr.addFlashAttribute("img", "uploadFiles/img/single/" + savedName);
             rttr.addFlashAttribute("singleFileDescription", singleFileDescription);
-        } catch(IOException e){
+        } catch (IOException e) {
 
             /* 설명. try 구문 안에서(DB를 다녀오는 business logic 처리) 예외가 발생했을 때,
-            *       파일만 올라가면 안되므로 찾아서 다시 지워줌. */
+             *       파일만 올라가면 안되므로 찾아서 다시 지워줌. */
             new File(filePath + "/" + savedName).delete();
         }
 
@@ -65,6 +65,58 @@ public class FileUploaderController {
 
     }
 
+    @PostMapping("multi-file")
+    public String multiFileUpload(@RequestParam List<MultipartFile> multiFiles,
+                                  @RequestParam String multiFileDescription,
+                                  RedirectAttributes rttr) throws IOException {
+//        System.out.println("multiFiles = " + multiFiles);
+//        System.out.println("multiFileDescription = " + multiFileDescription);
+
+        String filePath = resourceLoader.getResource("classpath:static/uploadFiles/img/multi")
+                                        .getFile().getAbsolutePath();
+
+        /* 설명. 사용자가 올린 파일들을 다 rename하고 저장하는 작업 및 DB로 전달하기 위한 List에 쌓기 */
+        List<Map<String, String>> files = new ArrayList<>();        // DB에 저장할 값들을 지닌 List
+        List<String> saveFiles = new ArrayList<>();                 // 화면단에서 img 태그가 참조할 정적 Resource 경로(src 경로)
+
+        try {
+            for (int i = 0; i < multiFiles.size(); i++) {
+                String originFileName = multiFiles.get(i).getOriginalFilename();
+                String ext = originFileName.substring(originFileName.lastIndexOf("."));
+                String savedName = UUID.randomUUID().toString().replace("-", "") + ext;
+
+                /* 설명. DB에 저장할 사용자가 올린 하나의 파일 정보 (DB 테이블 컬럼을 참고할 것) */
+                Map<String, String> file = new HashMap<>();
+                file.put("originFileName", originFileName);
+                file.put("savedName", savedName);
+                file.put("filePath", filePath);
+                file.put("multiFileDescription", multiFileDescription);
+
+                files.add(file);
+
+                multiFiles.get(i).transferTo(new File(filePath + "/" + savedName));
+                saveFiles.add("uploadFiles/img/multi/" + savedName);
+            }
+
+            /* 설명. 여기까지 문제가 없었다면 화면에 재료를 던져준다. */
+            rttr.addFlashAttribute("message", "다중 파일 업로드 성공!");
+            rttr.addFlashAttribute("imgs", saveFiles);
+            rttr.addFlashAttribute("multiFileDescription", multiFileDescription);
+        } catch(Exception e){
+
+            /* 설명. List<Map<String, String>> files로 쌓인 업로드 된 파일들을 찾아 일일이 다시 지운다.*/
+            for(int i = 0; i < files.size(); i++){
+                Map<String, String> file = files.get(i);
+                new File(filePath + "/" + file.get("savedName")).delete();
+            }
+
+            /* 설명. 실패 했다면 실패 메시지를 전달한다.*/
+            rttr.addFlashAttribute("message", "파일 업로드 실패!");
+        }
+        return "redirect:/result";
+    }
+
     @GetMapping("result")
-    public void result(){}
+    public void result() {
+    }
 }
